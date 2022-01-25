bb34ebfe0bf7bb27fe676664ea8dfa4d92e2646a
mariyasada <mariyasada2406@gmail.com>
2022-01-25 20:50:38 +0530

    adding js to btn

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const alertPrimaryBtn = document.querySelector("#alert-primary-btn");
-const alertPrimaryDiv = document.querySelector("#alert-primary-code");
+const alertPrimaryBtn = document.querySelector("#alert-code-btn");
+const alertPrimaryDiv = document.querySelector("#alert-div-code");
 
 alertPrimaryBtn.addEventListener("click",()=>{
     alertPrimaryDiv.style.display =="block"?alertPrimaryDiv.style.display ="none":alertPrimaryDiv.style.display ="block";
