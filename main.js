f6a199d10c56004bf2ee146bea311d18365a1cbd
mariyasada <mariyasada2406@gmail.com>
2022-01-26 23:04:25 +0530

    adding some more stuff

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const alertPrimaryDiv = document.querySelector("#alert-div-code");
 const avatarCodeBtn = document.querySelector("#avatar-code-btn");
 const avatarDiv = document.querySelector("#avatar-div-code");
 
-alertPrimaryBtn.addEventListener("click",()=>{
-    alertPrimaryDiv.style.display =="block"?alertPrimaryDiv.style.display ="none":alertPrimaryDiv.style.display ="block";
-})
+alertPrimaryBtn.addEventListener("click",() =>{
+    alertPrimaryDiv.style.display == "block"? alertPrimaryDiv.style.display ="none": alertPrimaryDiv.style.display ="block";
+});
 
 //  avatar code start here............................
-avatarCodeBtn.addEventListener("click",()=>{
-    console.log("clicked");
-    avatarDiv.style.display =="block"?avatarDiv.style.display ="none":avatarDiv.style.display ="block";
-})
+// avatarCodeBtn.addEventListener("click",() =>{
+//     console.log("clicked");
+//     avatarDiv.style.display =="block" ? avatarDiv.style.display ="none": avatarDiv.style.display ="block";
+// });
