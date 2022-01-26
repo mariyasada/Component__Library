30f66481c16000eab4f3ad239c48c1b2b0dca0db
mariyasada <mariyasada2406@gmail.com>
2022-01-26 17:50:01 +0530

    done with avatar componnet

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const alertPrimaryBtn = document.querySelector("#alert-code-btn");
 const alertPrimaryDiv = document.querySelector("#alert-div-code");
+const avatarCodeBtn = document.querySelector("#avatar-code-btn");
+const avatarDiv = document.querySelector("#avatar-div-code");
 
 alertPrimaryBtn.addEventListener("click",()=>{
     alertPrimaryDiv.style.display =="block"?alertPrimaryDiv.style.display ="none":alertPrimaryDiv.style.display ="block";
+})
+
+//  avatar code start here............................
+avatarCodeBtn.addEventListener("click",()=>{
+    console.log("click");
+    avatarDiv.style.display =="block"?avatarDiv.style.display ="none":avatarDiv.style.display ="block";
 })
